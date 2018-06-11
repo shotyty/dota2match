7b7557e629e9e05204a40aafb2dfa0871358dfd5
shotyty <newhshoty@yahoo.com>
2018-06-11 10:48:38 -0400

    3rd

--- deleteafter.js
+++ deleteafter.js
@@ -5071,7 +5071,7 @@ var heroAbilities = [
 
 
 
-fetch('http://72.11.39.21:3000/api/v1/players')
+fetch('http://192.168.1.3:3000/api/v1/players')
   .then(function(response) {
     return response.json();
   })
